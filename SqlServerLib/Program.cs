34af301eba0a9cac3beb055ecf2e7b664320991b
Greg Doud <gpdoud@gmail.com>
2021-07-06 16:37:23 -0400

    Add project files.

---
+++ SqlServerLib/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using SqlServerLibrary;
+
+namespace SqlServerConsole {
+    class Program {
+        static void Main(string[] args) {
+
+            SqlServerLib ssl = new SqlServerLib();
+            ssl.Connect("localhost\\sqlexpress", "PrsDb31");
+            ssl.Disconnect();
+        }
+    }
+}
---
+++ SqlServerLibrary/SqlServerLib.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.SqlClient;
+
+using System;
+
+namespace SqlServerLibrary {
+
+    public class SqlServerLib {
+
+        SqlConnection sqlConn;
+
+        public void Connect(string server, string database) {
+            var connStr = $"server={server};" 
+                            + $"database={database};"
+                            + $"trusted_connection=true;";
+            sqlConn = new SqlConnection(connStr);
+            sqlConn.Open();
+            if(sqlConn.State != System.Data.ConnectionState.Open) {
+                throw new Exception("Connection did not open!");
+            }
+
+        }
+        public void Disconnect() {
+            if(sqlConn.State == System.Data.ConnectionState.Open) {
+                sqlConn.Close();
+            }
+        }
+    }
+}
