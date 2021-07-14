ccdd5bc9a75de40e5dcc3671e5e9ca74ff4c2fde
Greg Doud <gpdoud@gmail.com>
2021-07-14 11:27:38 -0400

    Committed by GitUtil

---
+++ SqlServerLibrary/Request.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SqlServerLibrary {
+    public class Request {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Description { get; set; }
+    }
+}
